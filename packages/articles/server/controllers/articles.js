16831444652bf98bd8d460fce0496d150648aa91
Eddie Dolan <dolan.charles06@gmail.com>
2015-03-12 16:26:35 -0500

    Removing unused functions from Controllers

--- packages/articles/server/controllers/articles.js
+++ packages/articles/server/controllers/articles.js
@@ -4,8 +4,7 @@
  * Module dependencies.
  */
 var mongoose = require('mongoose'),
-  Article = mongoose.model('Article'),
-  _ = require('lodash');
+  Article = mongoose.model('Article');
 
 
 /**
@@ -20,64 +19,13 @@ exports.article = function(req, res, next, id) {
   });
 };
 
-/**
- * Create an article
- */
-exports.create = function(req, res) {
-  var article = new Article(req.body);
-  article.user = req.user;
-
-  article.save(function(err) {
-    if (err) {
-      return res.status(500).json({
-        error: 'Cannot save the article'
-      });
-    }
-    res.json(article);
-
-  });
-};
-
-/**
- * Update an article
- */
-exports.update = function(req, res) {
-  var article = req.article;
-
-  article = _.extend(article, req.body);
-
-  article.save(function(err) {
-    if (err) {
-      return res.status(500).json({
-        error: 'Cannot update the article'
-      });
-    }
-    res.json(article);
-
-  });
-};
-
-/**
- * Delete an article
- */
-exports.destroy = function(req, res) {
-  var article = req.article;
-
-  article.remove(function(err) {
-    if (err) {
-      return res.status(500).json({
-        error: 'Cannot delete the article'
-      });
-    }
-    res.json(article);
-
-  });
-};
 
 /**
  * Show an article
  */
 exports.show = function(req, res) {
+
+  // PAULO WILLS SEND US CODE
   res.json(req.article);
 };
 
@@ -85,13 +33,6 @@ exports.show = function(req, res) {
  * List of Articles
  */
 exports.all = function(req, res) {
-  Article.find().sort('-created').populate('user', 'name username').exec(function(err, articles) {
-    if (err) {
-      return res.status(500).json({
-        error: 'Cannot list the articles'
-      });
-    }
-    res.json(articles);
-
-  });
+  // PAULO WILL SEND US CODE
+    res.json(req.articles);
 };
