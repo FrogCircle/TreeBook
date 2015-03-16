ecc5eebfb7ee6b4f984fa83f9ef321a6698d18b0
Jonah Nisenson <jsnisenson@gmail.com>
2015-03-16 18:30:40 -0500

    update DB query to get treeid and update CSS for navbar
    1. include tree.treeid in getAll and getTreeData query requests in server/controllers/article.js
    2. update article.css in assets folder for green gradient top navbar

--- packages/articles/server/controllers/articles.js
+++ packages/articles/server/controllers/articles.js
@@ -10,7 +10,7 @@ exports.getTreeData = function(req, res) {
   console.log('in getTreeData');
   var treeid = req.params.treeId;
   pg.connect(conString, function(err, client, done) {
-    var selectMessages = 'SELECT tree.name, q.qspecies, tree.plantdate, l.latitude, l.longitude, image.imageurl, image.imagewidth, image.imageheight, image.imagetype from qspecies q JOIN tree ON (q.qspeciesid = tree.qspeciesid) JOIN "location" l ON (l.locationid = tree.locationid) JOIN image ON (q.qspeciesid = image.qspeciesid) WHERE treeid = $1;';
+    var selectMessages = 'SELECT tree.name, tree.treeid, q.qspecies, tree.plantdate, l.latitude, l.longitude, image.imageurl, image.imagewidth, image.imageheight, image.imagetype from qspecies q JOIN tree ON (q.qspeciesid = tree.qspeciesid) JOIN "location" l ON (l.locationid = tree.locationid) JOIN image ON (q.qspeciesid = image.qspeciesid) WHERE treeid = $1;';
     client.query(selectMessages, [treeid], function(error, results) {
       done();
       if (error) {
@@ -29,7 +29,8 @@ exports.getAll = function(req, res) {
   console.log('in get All');
   pg.connect(conString, function(err, client, done) {
     console.log(err);
-    var selectTrees = 'SELECT tree.name, q.qspecies, l.latitude, l.longitude, thumbnail.url, thumbnail.width, thumbnail.height, thumbnail.contenttype FROM qspecies q JOIN tree ON (q.qspeciesid = tree.qspeciesid) JOIN "location" l ON (l.locationid = tree.locationid) JOIN thumbnail ON (q.qspeciesid = thumbnail.qspeciesid) LIMIT 250;';    client.query(selectTrees, function(error, results) {
+    var selectTrees = 'SELECT tree.name, tree.treeid, q.qspecies, l.latitude, l.longitude, thumbnail.url, thumbnail.width, thumbnail.height, thumbnail.contenttype FROM qspecies q JOIN tree ON (q.qspeciesid = tree.qspeciesid) JOIN "location" l ON (l.locationid = tree.locationid) JOIN thumbnail ON (q.qspeciesid = thumbnail.qspeciesid) LIMIT 250;';
+    client.query(selectTrees, function(error, results) {
     }, function(error, results) {
       done();
       if(error) {
@@ -38,7 +39,7 @@ exports.getAll = function(req, res) {
       console.log(results.rows);
       res.json(results.rows);
     });
-  })
+  });
 };
 
 
