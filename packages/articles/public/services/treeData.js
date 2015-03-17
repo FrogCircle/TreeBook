8ea1a3cf9df9bdb7a08a443b746b78ba7320b1ac
drceric <drc.ericlau@gmail.com>
2015-03-17 02:17:54 -0400

    factor out the service from controller

---
+++ packages/articles/public/services/treeData.js
@@ -0,0 +1,21 @@
+'use strict';
+
+angular.module('mean.articles')
+
+.service('treeData', ['Articles', '$stateParams',
+  function(Articles, $stateParams){
+    var tree = null;
+    console.log($stateParams.treeId);
+    var promise = Articles.get({
+      treeId: $stateParams.treeId
+      }, function(t) {
+        tree = t;
+      });
+    return {
+      promise:promise,
+      getTree: function(){
+        return tree;
+      }
+    };
+  }
+]);
