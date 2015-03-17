25b7aaa507b433a1e74d2e842ab37fa49322c1f9
drceric <drc.ericlau@gmail.com>
2015-03-17 02:19:07 -0400

    factor out service from controller, comment out

--- packages/articles/public/controllers/articles.js
+++ packages/articles/public/controllers/articles.js
@@ -24,21 +24,21 @@ angular.module('mean.articles')
       });
     };
   }
-])
-
-.service('treeData', ['Articles', '$stateParams', function(Articles, $stateParams){
-  var tree = null;
-  console.log($stateParams.treeId);
-  var promise = Articles.get({
-      treeId: $stateParams.treeId
-    }, function(t) {
-      tree = t;
-    });
-  return {
-    promise:promise,
-    getTree: function(){
-      return tree;
-    }
-  };
-}
 ]);
+
+// .service('treeData', ['Articles', '$stateParams', function(Articles, $stateParams){
+//   var tree = null;
+//   console.log($stateParams.treeId);
+//   var promise = Articles.get({
+//       treeId: $stateParams.treeId
+//     }, function(t) {
+//       tree = t;
+//     });
+//   return {
+//     promise:promise,
+//     getTree: function(){
+//       return tree;
+//     }
+//   };
+// }
+// ]);
