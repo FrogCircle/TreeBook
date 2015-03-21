c5357c2d1b92b4cac1129e6aba31de12902c0168
drceric <drc.ericlau@gmail.com>
2015-03-21 18:28:24 -0400

    search

--- packages/articles/public/services/search.js
+++ packages/articles/public/services/search.js
@@ -1,10 +1,34 @@
-// 'use strict';
+'use strict';
+/*
+  Factory that handle the address search in the list.html
+*/
+angular.module('mean.articles')
 
-// angular.module('mean.articles')
+.factory('Search', ['uiGmapGoogleMapApi',
+  function(uiGmapGoogleMapApi){
+    /*
+      Take the address as params and return the location(lat, lng)
+      should remember check the validation of the input somewhere
+      @params string target
+      @result
+    */
+    var getLocation = function(target) {
+      return uiGmapGoogleMapApi.then(function(maps){
+        var geocoder = new maps.Geocoder();
+        var request = { address: target };
+        geocoder.geocode(request, function(results, status){
+          // location is found
+          if (status === maps.GeocoderStatus.OK) {
+            console.log(results[0].geometry.location);
+            var location = results[0].geometry.location;
+            return location;
+          } else {
+            console.log('No Valid Address Found: ' + status);
+          }
+        });
+      });
+    };
 
-// .factory('Search', function(){
-//   var searchLocation = function(){
-
-//   };
-
-// });
+    return { getLocation: getLocation };
+  }
+]);
