629c46eee3cdb546e133cb02e7005524ae8a9f5c
drceric <drc.ericlau@gmail.com>
2015-03-22 13:23:47 -0400

    basic frame for search finished

--- packages/articles/public/controllers/paginationController.js
+++ packages/articles/public/controllers/paginationController.js
@@ -10,15 +10,26 @@ angular.module('mean.articles')
     var itemsPerPage = 25;
     $scope.currentPage = 1;
     $scope.treees = [];
+
+    //Factor out the pagination function to be reused for all the methods
+    var paginateTree = function(trees){
+      $scope.totalItems = trees.length / itemsPerPage * 8;
+      for (var i = 0; i < $scope.totalItems; i = i + 1){
+        $scope.treees.push(trees.slice(i * itemsPerPage, (i + 1) * itemsPerPage));
+      }
+      $scope.trees = trees;
+    }
+
     // Helper method to call Trees factory to get all trees
     $scope.find = function() {
       console.log('find has been called.');
       Trees.query(function(trees) {
-        $scope.totalItems = trees.length / itemsPerPage * 8;
-        for (var i = 0; i < $scope.totalItems; i = i + 1){
-          $scope.treees.push(trees.slice(i * itemsPerPage, (i + 1) * itemsPerPage));
-        }
-        $scope.trees = trees;
+        // $scope.totalItems = trees.length / itemsPerPage * 8;
+        // for (var i = 0; i < $scope.totalItems; i = i + 1){
+        //   $scope.treees.push(trees.slice(i * itemsPerPage, (i + 1) * itemsPerPage));
+        // }
+        // $scope.trees = trees;
+        paginateTree(trees);
       });
     };
 
@@ -36,15 +47,17 @@ angular.module('mean.articles')
       var lng = location.lng;
       if(lat <= -122.368107024455 && lat >= -122.511257155794 && lng <=  37.8103949467147 && lng >=  37.5090039879895) {
         //Search by location
-        var body = { longitude: lng, latitude: lat};
-        Search.getNearTrees({ search: body }, function(results){
+        var body = { longitude: lng, latitude: lat };
+        Search.getNearTrees.get({ search: body }, function(results){
           console.log(results);
+          paginateTree(results);
         })
       } else {
         //search by name
         Search.getByName.get({ search: searchString }, function(results){
           console.log(results);
           //add the results to the page
+          paginateTree(results);
         })
       }
     };
